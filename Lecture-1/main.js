934234a8a50d461f7f932dcb241ee97ccbd68e74
Ujwala Lute <luteujwala@gmail.com>
2023-05-17 19:23:48 +0530

    First Lecture

---
+++ Lecture-1/main.js
@@ -0,0 +1,5 @@
+console.log('hello javascript');
+console.log('again printing')
+
+/*alert("hello");
+[1,2],forEach(alert);*/
---
+++ Lecture-1/variables.js
@@ -0,0 +1,8 @@
+let result = 10;
+console.log[result];
+
+result = "hello";
+console.log[result];
+
+result = true;
+console.log[result];
